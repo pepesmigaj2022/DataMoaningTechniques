5f2dacfe10b102ff532fea112afcf88d1251c203
victor-xite <victor@xite.com>
2025-04-08 13:37:44 +0200

    Added questions and some things

--- user.py
+++ user.py
@@ -3,13 +3,26 @@ import pandas as pd
 class User:
     def __init__(self, uid, df):
        
+
         self.id = uid
         self.df = df
+       
+        # Should we include 'time' as an input?
+        # Because to predict the mood of a User at a time,
+        # We can only use data up to the day (not time) before
+
+        # What are we trying to p[redict?
+        #   - Avg mood for the next day
+        #   - Mood for the next hour
 
         self.data_history = df[df['id']==self.id]
         self.mood_history = self.data_history[self.data_history['record_type']=='mood']
         self.usage_history = self.data_history[self.data_history['record_type']!='mood']
 
+        # Mean daily mood
+        # Aggregated (additive) daily usage
+        # 7day_ma_mood
+        # 7day_ma_usage
         
     def __repr__(self):
         return (f"User(id={self.id}, df_shape={self.df.shape}, "
