437d413011c5d97fd7c6b5560869d62a77bec234
martinanfi <126090589+martinanfi@users.noreply.github.com>
2025-04-18 11:12:43 +0200

    clean file

--- features.py
+++ features.py
@@ -1,18 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+
+
+####################################################################################
+# FEATURES                                                                         #
+####################################################################################
+
 
 def feature_general_op(id, record_type, op, df, record_detail = None):
     ''' 
     works when the operation of the values can be one function stored in op such as
     np.mean, np.sum... Variable record_type is a string from the column dataset_clean.csv
-    It gives 2 vectors: one containing the dates and the other the corresponding 
+    It gives 2 vectors: one containing the dates (days) and the other the corresponding 
     values per day per id.
     '''
     if record_detail is None:
         filtered_df = df[(df['record_type'] == record_type)& (df['id'] == id)]
     else:
         filtered_df = df[(df['record_type'] == record_type) & (df['id'] == id) & (df['record_detail'] == record_detail)]
+
+    if filtered_df.empty:
+        print(f"No data for id {id} with record_type {record_type} and record_detail {record_detail}")
+        return [[], []]  # Return empty lists if no data is found
 
     vals = [filtered_df['date'].values, filtered_df['value'].values]
     result_vals = []
@@ -84,9 +93,41 @@ def feature_gaming(id, df):
     op = np.sum
     return feature_general_op(id, record_type, op, df, record_detail=record_detail)
 
-file2 = "dataset_clean.csv"
-dataset_clean = pd.read_csv(file2)
 
-vec = feature_gaming(1,dataset_clean)
-print(vec)
+####################################################################################
+# DATAFRAME PER PERSON PER DAY                                                     #
+####################################################################################
+
+def create_df(id, df):
+    # have to update when creating more features
+    features = [feature_avg_mood, feature_var_mood, feature_screen_time, feature_apps_time, 
+                feature_avg_arousal, feature_var_arousal, feature_social_contact, feature_gaming]
+    feature0 = features[0]
+    dfs = pd.DataFrame({
+        'date' : feature0(id, df)[0],
+        f'{feature0}'.split()[1] : feature0(id, df)[1]
+    })
+
+    for feature in features[1:]:
+        df_new = pd.DataFrame({
+            'date' : feature(id, df)[0],
+            f'{feature}'.split()[1] : feature(id, df)[1]
+        })
+        dfs = pd.merge(dfs, df_new, on='date', how='outer')
+        if dfs.empty:
+            return
+        dfs.to_csv(f'features/output{id}.csv')
+
+
+####################################################################################
+####################################################################################
+
+
+file = "dataset_clean.csv"
+dataset_clean = pd.read_csv(file)
+num_people = max(dataset_clean['id'])
+
+for i in range(num_people+1):
+    create_df(i+1, dataset_clean)
+
 
