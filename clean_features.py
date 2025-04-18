cc4d7cf31aa813b36ba7336ee0f707d3335d6e35
martinanfi <126090589+martinanfi@users.noreply.github.com>
2025-04-18 15:15:02 +0200

    features bitch

---
+++ clean_features.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import os
+
+def clean_features_df(df, file_name):
+    df = df.dropna(subset=['feature_avg_mood', 'feature_var_mood'])
+
+    for col in ['feature_avg_arousal', 'feature_var_arousal']:
+        df[col] = df[col].fillna(df[col].mean())
+
+    for col in ['feature_screen_time', 'feature_apps_time', 'feature_social_contact', 'feature_gaming']:
+        df[col] = df[col].fillna(0)
+
+    df.to_csv(f'clean_features/clean_{file_name}')
+
+    
+if __name__ == '__main__':
+    folder = 'features'
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+        df = pd.read_csv(file_path)
+
+        clean_features_df(df, file)
+
